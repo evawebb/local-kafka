f76987950d147db25b4a40462aaf593efd718da5
Zack Webb <Zachary.Webb@ca.com>
2017-09-25 14:45:24 -0600

    Initial commit.

---
+++ run.sh
@@ -0,0 +1,58 @@
+NETWORK_NAME='local-kafka-net'
+NODE_NAME='local-kafka-node'
+ZK_NAME='local-kafka-zk'
+BROKERS=3
+
+NETWORK_IP_RANGE='172.39.39.0/24'
+NODE_IP_PREFIX='172.39.39.1'
+ZK_IP='172.39.39.200'
+
+echo 'Deleting existing docker containers...'
+for i in `seq 1 $BROKERS`; do
+  docker rm -f "$NODE_NAME-$i"
+done
+docker rm -f "$ZK_NAME"
+echo 'Deleting existing docker network...'
+docker network rm "$NETWORK_NAME"
+
+echo 'Building docker images...'
+NODE_CONTAINER_ID=$( \
+  docker build -t "$NODE_NAME" ./kafka_node/ | \
+  tee /dev/tty)
+ZK_CONTAINER_ID=$( \
+  docker build -t "$ZK_NAME" ./zookeeper/ | \
+  tee /dev/tty)
+
+NODE_CONTAINER_ID=$(echo "$NODE_CONTAINER_ID" | awk '/^Successfully built/ {print $3}')
+ZK_CONTAINER_ID=$(echo "$ZK_CONTAINER_ID" | awk '/^Successfully built/ {print $3}')
+
+echo 'Creating new docker network...'
+echo "Network ID: $( \
+  docker network create \
+    --subnet "$NETWORK_IP_RANGE" \
+    "$NETWORK_NAME")"
+
+echo 'Running docker containers...'
+for i in `seq 1 $BROKERS`; do
+  cat kafka_node/server.properties | \
+    sed "s/broker.id=0/broker.id=$i/" | \
+    sed "s/172.39.39.11/$NODE_IP_PREFIX$i/" | \
+    sed "s/9092/909$i/" > \
+    "kafka_node/server.$i.properties"
+  echo "Kafka node container ID: $( \
+    docker run -dit \
+      --name "$NODE_NAME-$i" \
+      --net "$NETWORK_NAME" \
+      --ip "$NODE_IP_PREFIX$i" \
+      -v "$(pwd)/kafka_node/server.$i.properties:/usr/share/kafka_2.11-0.11.0.1/config/server.properties" \
+      -p "909$i:909$i" \
+      "$NODE_CONTAINER_ID")"
+done
+echo "Zookeeper container ID: $( \
+  docker run -dit \
+  --name "$ZK_NAME" \
+  --net "$NETWORK_NAME" \
+  --ip "$ZK_IP" \
+  -p 2181:2181 \
+  "$ZK_CONTAINER_ID")"
+
