0b5076f2f8a8bf7f7d688cf49ce9b393986ef3f5
Zack Webb <Zachary.Webb@ca.com>
2017-09-26 09:21:13 -0600

    Working with one broker, WIP on multiple brokers.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 NETWORK_NAME='local-kafka-net'
 NODE_NAME='local-kafka-node'
 ZK_NAME='local-kafka-zk'
-BROKERS=3
+BROKERS=1
 
 NETWORK_IP_RANGE='172.39.39.0/24'
 NODE_IP_PREFIX='172.39.39.1'
@@ -17,7 +17,7 @@ docker network rm "$NETWORK_NAME"
 
 echo 'Building docker images...'
 NODE_CONTAINER_ID=$( \
-  docker build -t "$NODE_NAME" ./kafka_node/ | \
+  docker build -t "$NODE_NAME" ./kafka-node/ | \
   tee /dev/tty)
 ZK_CONTAINER_ID=$( \
   docker build -t "$ZK_NAME" ./zookeeper/ | \
@@ -34,19 +34,20 @@ echo "Network ID: $( \
 
 echo 'Running docker containers...'
 for i in `seq 1 $BROKERS`; do
-  cat kafka_node/server.properties | \
+  cat kafka-node/server.properties | \
     sed "s/broker.id=0/broker.id=$i/" | \
-    sed "s/172.39.39.11/$NODE_IP_PREFIX$i/" | \
-    sed "s/9092/909$i/" > \
-    "kafka_node/server.$i.properties"
+    sed "s/172.39.39.11/$NODE_IP_PREFIX$i/" > \
+    "kafka-node/server.$i.properties"
+    # sed "s/9092/909$i/" | \
   echo "Kafka node container ID: $( \
     docker run -dit \
       --name "$NODE_NAME-$i" \
       --net "$NETWORK_NAME" \
       --ip "$NODE_IP_PREFIX$i" \
-      -v "$(pwd)/kafka_node/server.$i.properties:/usr/share/kafka_2.11-0.11.0.1/config/server.properties" \
-      -p "909$i:909$i" \
+      -v "$(pwd)/kafka-node/server.$i.properties:/usr/share/kafka_2.11-0.11.0.1/config/server.properties" \
+      -p "9092:9092" \
       "$NODE_CONTAINER_ID")"
+      # -p "909$i:909$i" \
 done
 echo "Zookeeper container ID: $( \
   docker run -dit \
